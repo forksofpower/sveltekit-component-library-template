ac6fd7bdc1f1d756e79041a7224d9ddaea78428c
Patrick Jones <forksofpower@gmail.com>
2023-01-15 19:47:51 -0600

    ignore tests, type declarations, and stories in svelte package

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,15 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  package: {
+    exports: (filePath) => !/^_|\/_|\.(d|test|stories)\.ts$/.test(filePath)
+  },
+  kit: {
+    adapter: adapter(),
+  }
 };
 
 export default config;
